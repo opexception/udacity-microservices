8eb42d07d14c853d5d13433e0e46a5eb750658da
Robert Maracle <rob.maracle@gmail.com>
2021-04-17 18:15:07 +0000

    filled out upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="opexception/udacity-projects:udacity-microservices"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username "opexception" &&\
+    docker image tag udacity-microservices $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push opexception/udacity-projects:udacity-microservices
