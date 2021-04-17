7e4d8bd577971e15312085155988e5629ac86d5d
Robert Maracle <rob.maracle@gmail.com>
2021-04-17 18:53:55 +0000

    improved check to make sure correct pod is running before running portforward command

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -20,11 +20,22 @@ kubectl get pods
 # Step 4:
 # Forward the container port to a host
 running=0
+firstcheck=0
 while [ $running -eq 0 ]
     do
-        if $(kubectl get pods | grep -q Running)
-            then running=1
-            else sleep 2
+        if $(kubectl get pods | grep 'udacity-microservices' | grep -q Running)
+            then 
+                echo -e "\n...Found running pod"
+                running=1
+            else
+                if [ $firstcheck -eq 0 ]
+                    then
+                        echo -n "Pod not ready yet."
+                        firstcheck=1
+                    else
+                        echo -n "."
+                fi
+                sleep 1
         fi
     done
 kubectl port-forward udacity-microservices 8000:80
